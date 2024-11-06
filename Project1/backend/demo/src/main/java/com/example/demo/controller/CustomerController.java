fcffc437eae8e40b2182a370aefe28d0dc1112c5
Jovan <jovan.jovovic064@gmail.com>
2024-11-06 15:33:47 +0100

    Project finished

--- Project1/backend/demo/src/main/java/com/example/demo/controller/CustomerController.java
+++ Project1/backend/demo/src/main/java/com/example/demo/controller/CustomerController.java
@@ -1,6 +1,12 @@
 package com.example.demo.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +15,12 @@ import com.example.demo.entity.Customer;
 import com.example.demo.service.CustomerService;
 
 import lombok.RequiredArgsConstructor;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class CustomerController {
     
     private final CustomerService customerService;
@@ -20,6 +28,47 @@ public class CustomerController {
     @PostMapping("/customer")
     public Customer postCustomer(@RequestBody Customer customer){
         return customerService.postCustomer(customer);
+    }
+
+    @GetMapping("/customers")
+    public List<Customer> getAllCustomers(){
+        return customerService.getAllCustomers();
+    }
+
+    @GetMapping("/customer/{id}")
+    public ResponseEntity<Customer> getCustomerById(@PathVariable Long id){
+        Customer customer= customerService.getCustomerById(id);
+        if(customer==null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(customer);
+
+    }
+
+    @PutMapping("/customer/{id}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable Long id, @RequestBody Customer customer){
+        Customer existingCustomer = customerService.getCustomerById(id);
+        if(existingCustomer==null){
+            return ResponseEntity.notFound().build();
+        }
+        existingCustomer.setName(customer.getName());
+        existingCustomer.setEmail(customer.getEmail());
+        existingCustomer.setPhone(customer.getPhone());
+
+         
+        Customer updatedCustomer =  customerService.updateCustomer(existingCustomer);
+        return ResponseEntity.ok().body(updatedCustomer);
+
+    }
+
+    @DeleteMapping("/customer/delete/{id}")
+    public ResponseEntity<Customer> deleteCustomer(@PathVariable Long id){
+        Customer customer = customerService.getCustomerById(id);
+        if(customer==null){
+            return ResponseEntity.notFound().build();
+        }
+        customerService.deleteCustomer(id);
+        return ResponseEntity.ok().build();
     }
 
 }
--- Project1/backend/demo/src/main/java/com/example/demo/service/CustomerService.java
+++ Project1/backend/demo/src/main/java/com/example/demo/service/CustomerService.java
@@ -6,6 +6,7 @@ import com.example.demo.entity.Customer;
 import com.example.demo.repository.CustomerRepository;
 
 import lombok.RequiredArgsConstructor;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +16,23 @@ public class CustomerService {
 
     public Customer postCustomer(Customer customer){
         return customerRepository.save(customer);
+    }
+
+
+    public List<Customer> getAllCustomers(){
+        return customerRepository.findAll();
+    }
+
+    public Customer getCustomerById(Long id){
+        return customerRepository.findById(id).orElse(null);
+    }
+
+    public Customer updateCustomer(Customer customer){
+        return customerRepository.save(customer);
+    }
+
+    public void deleteCustomer(Long id){
+        customerRepository.deleteById(id);
     }
 
 }
